0212c709fe7f13a8f854b5afcb97a9f88c844edf
Kaua-Rossi <kauarossipersonal@gmail.com>
2026-05-07 13:03:19 -0300

    feat: updates deletePost logic
    Removes the unnecessary logic of injecting the GetPostById in the DeletePostController, and do all the logic only in the handler.

--- VSAProject.API/Features/DeletePost/DeletePostController.cs
+++ VSAProject.API/Features/DeletePost/DeletePostController.cs
@@ -5,22 +5,14 @@ namespace VSAProject.API.Features.DeletePost
 {
     [ApiController]
     [Route("api/posts")]
-    public class DeletePostController(DeletePostHandler deletePostHandler, GetPostByIdHandler getPostByIdHandler) : ControllerBase
+    public class DeletePostController(DeletePostHandler handler) : ControllerBase
     {
-        private readonly GetPostByIdHandler _getPostByIdHandler = getPostByIdHandler;
-        private readonly DeletePostHandler _deletePostHandler = deletePostHandler;
+        private readonly DeletePostHandler _handler = handler;
 
         [HttpDelete("{id}", Name = "DeletePost")]
         public async Task<IActionResult> DeletePost(int Id)
         {
-            var post = await _getPostByIdHandler.Handle(Id);
-
-            if (post == null)
-            {
-                return NotFound();
-            }
-
-            await _deletePostHandler.Handle(Id);
+            await _handler.Handle(Id);
             return NoContent();
         }
     }
--- VSAProject.API/Features/DeletePost/DeletePostHandler.cs
+++ VSAProject.API/Features/DeletePost/DeletePostHandler.cs
@@ -6,11 +6,16 @@ namespace VSAProject.API.Features.DeletePost
     {
         private readonly AppDbContext _db = db;
 
-        public async Task Handle(int id)
+        public async Task<bool> Handle(int id)
         {
             var post = _db.Posts.First(p => p.Id == id);
+            if (post == null)
+            {
+                return false;
+            }
             _db.Remove(post);
             await _db.SaveChangesAsync();
+            return true;
         }
     }
 }
